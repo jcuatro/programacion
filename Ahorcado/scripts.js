eaed79bd8e214d68a1f81fbb781951c3e6f7320a
ALUMNADO\jurapu <julioramirezpuig98@gmail.com>
2017-10-05 10:18:49 +0200

    Hola que tal

---
+++ Ahorcado/scripts.js
@@ -0,0 +1,10 @@
+var holi="j";
+console.log(holi);
+var coso=prompt("Introduce una letra");
+
+while(holi!=coso){
+  var coso=prompt("Intenta meterla otra vez, la letra");
+
+}
+alert("Has dado en el clavo marinero");
+console.log("funciona");
